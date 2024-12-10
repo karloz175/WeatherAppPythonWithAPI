49f2afd758b4a78650ffc88c81768e1b022aceff
karloz175 <karloz175@gmail.com>
2024-12-10 15:06:44 +0100

    city from telegram bot and sending respond on telegram chat

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import requests
 
+#API:
 API_Key = ""
 
+#TELEGRAM BOT:
+TOKEN = ""
+CHAT_ID = ""
 
+# Checking chat id:
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates" 
+result = requests.get(url).json()['result']
+city_name = result[len(result)-1]['message']['text'] # - taking city name from last telegram message to bot
 city_coordinates = {}
 
-do_we_check_another_city = True
-while(do_we_check_another_city):
-    try:
-        city_name = input("In which city you would like to check temperature?")
-        response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=5&appid={API_Key}")
+def send_message(mssg):
+    requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={mssg}")
+
+
+try:
+    #city_name = input("In which city you would like to check temperature?") # - checking name of city from console
+    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=5&appid={API_Key}")
+    if response.status_code == 200:
+        data = response.json()
+        city_coordinates.update({'lat':data[0]['lat'], 'lon': data[0]['lon']})
+        
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={city_coordinates['lat']}&lon={city_coordinates['lon']}&appid={API_Key}&units=metric")
+
         if response.status_code == 200:
             data = response.json()
-            city_coordinates.update({'lat':data[0]['lat'], 'lon': data[0]['lon']})
-            
-            response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={city_coordinates['lat']}&lon={city_coordinates['lon']}&appid={API_Key}&units=metric")
-
-            if response.status_code == 200:
-                data = response.json()
-                print("In " + str(data['name']) + " the currenly weather is: " + str(data['weather'][0]['description']) + ".")
-                print("Temperature is: " + str(data['main']['temp']) + " C°")
-        else:
-            print("Site is not working, response code: " + str(response.status_code))
-        check = input("Do you want to check another's city weather?(Type \"no\" to end the program)")
-        if check == "no":
-            do_we_check_another_city = False
-    except IndexError:
-        print("Wrong city name, please enter correct one!")
+            message = "In " + city_name + " the currenly weather is: " + str(data['weather'][0]['description']) + ".\nTemperature is: " + str(data['main']['temp']) + " C°"
+            send_message(message)
+    else:
+        message = "Site is not working, response code: " + str(response.status_code)
+        send_message(message)
+except IndexError:
+    message = "Wrong city name, please enter correct one!"
+    send_message(message)
         
